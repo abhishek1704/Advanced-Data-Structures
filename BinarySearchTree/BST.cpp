f93f40dc0fcc472a4f274f7080e19f03d4f30afd
abhishek1704 <velankarabhishek@gmail.com>
2020-04-27 10:35:20 +0530

    Add files via upload

---
+++ BinarySearchTree/BST.cpp
@@ -0,0 +1,345 @@
+
+#include <iostream>
+#define MAX 50
+using namespace std;
+
+class BST;
+
+class node
+{
+	node *left,*right;
+	int data;
+
+public:
+	friend class BST;
+};
+
+class BST
+{
+	node *root;
+	node *q[MAX];
+	int f,r;
+public:
+	BST();
+	void createBST();
+	void insert();
+	void display();
+	void inorder(node*);
+	void search(int,node**,node**);
+	void delet(int);
+	BST createMirror();
+	node* mirrorImg(node*);
+	void displayLevelWise();
+};
+
+BST::BST()
+{
+	root = NULL;
+	f = r = -1;
+}
+
+void BST::createBST()
+{
+	int n;
+	cout<<"Enter the number of nodes you want to create: ";
+	cin>>n;
+
+	for(int i=0;i<n;i++)
+		insert();
+
+}
+void BST::insert()
+{
+	node *p = root;
+	int data;
+	if(root==NULL)
+	{
+		p = new node;
+		p->left=p->right=NULL;
+		cout<<"Enter data for the root: ";
+		cin>>p->data;
+		root = p;
+		return;
+	}
+
+	cout<<"Enter a data: ";
+	cin>>data;
+	do
+	{
+		if(data<=p->data)
+		{
+			if(p->left==NULL)
+			{
+				p->left=new node;
+				(p->left)->left=(p->left)->right=NULL;
+				(p->left)->data=data;
+				return;
+			}
+			else
+				p = p->left;
+		}
+		else if(data>p->data)
+		{
+
+			if(p->right==NULL)
+			{
+				p->right=new node;
+				(p->right)->left=(p->right)->right=NULL;
+				(p->right)->data=data;
+				return;
+			}
+			else
+				p = p->right;
+		}
+
+	}while(1);
+}
+
+void BST::display()
+{
+	if(root==NULL)
+		cout<<"The tree is empty !"<<endl;
+	else
+		inorder(root);
+	cout<<endl;
+}
+
+void BST::inorder(node *p)
+{
+	if(p!=NULL)
+	{
+		inorder(p->left);
+		cout<<p->data<<" ";
+		inorder(p->right);
+	}
+
+	return;
+}
+
+void BST::search(int key,node **loc,node **par)
+{
+	if(root==NULL)
+	{
+		cout<<"The tree is empty !!"<<endl;
+		return;
+	}
+	node *temp=root;
+	while(temp!=NULL)
+	{
+		if(key<temp->data)
+		{
+			*par = temp;
+			temp = temp->left;
+		}
+		else if(key>temp->data)
+		{
+			*par = temp;
+			temp = temp->right;
+		}
+		else
+		{
+			*loc = temp;
+			cout<<"Data found !!"<<endl;
+			return;
+		}
+	}
+
+	if(temp==NULL)
+	{
+		cout<<"Data not found !!"<<endl;
+		loc = NULL;
+	}
+}
+
+void BST::delet(int key)
+{
+	node *temp,*loc,*par;
+
+	if(root==NULL) // Check if tree is empty
+	{
+		cout<<"The tree is empty !!";
+		return;
+	}
+
+	if(root->data==key && root->left==NULL && root->right==NULL) // To delete the root if key is equal to the root
+	{
+		temp = root;
+		root = NULL;
+		delete temp;
+		cout<<"Now tree is empty !!"<<endl;
+		return;
+	}
+
+	search(key,&loc,&par); // Search a key and get the location of node and its parent
+
+
+	if(loc==NULL) // Check if key not found
+	{
+		cout<<"The key not found !"<<endl;
+		return;
+	}
+
+	if(loc->right==NULL && loc->left==NULL) // To delete the leaf node
+	{
+		if(loc==par->left)
+			par->left = NULL;
+		else
+			par->right = NULL;
+
+		temp = loc;
+		delete temp;
+		return;
+	}
+
+	if(loc->left!=NULL && loc->right==NULL) 	// To delete the node having single child
+	{
+		if(par->left==loc)
+			par->left=loc->left;
+		else
+			par->right = loc->left;
+
+		temp = loc;
+		delete temp;
+		return;
+	}
+	else if(loc->left==NULL && loc->right!=NULL)
+	{
+		if(par->left==loc)
+			par->left = loc->right;
+		else
+			par->left = loc->right;
+
+		temp = loc;
+		delete temp;
+		return;
+	}
+
+	if(loc->right!=NULL && loc->left!=NULL) // To delete the node having two children
+	{
+		int temp1 = loc->data;
+		loc->data = (loc->right)->data;
+		(loc->right)->data = temp1;
+
+
+		loc->right = NULL;
+		temp = loc->right;
+		delete temp;
+	}
+}
+
+BST BST::createMirror()
+{
+	BST tm;
+	tm.root = mirrorImg(root);
+	return tm;
+}
+
+node* BST::mirrorImg(node* p)
+{
+
+	if(p==NULL)
+		return p;
+
+	node *temp = new node;
+	temp->data = p->data;
+	temp->left = mirrorImg(p->right);
+	temp->right = mirrorImg(p->left);
+
+	return temp;
+}
+
+void BST::displayLevelWise()
+{
+	node* temp;
+	if(root==NULL)
+	{
+		cout<<"Tree is empty !"<<endl;
+		return;
+	}
+
+	f++;r++;
+	q[r] = root;
+
+	while(r!=f-1)
+	{
+		temp = q[f];
+		f++;
+		cout<<temp->data<<" ";
+		if(temp->left)
+		{
+			r++;
+			q[r] = temp->left;
+		}
+		if(temp->right)
+		{
+			r++;
+			q[r] = temp->right;
+		}
+	}
+}
+
+int main() {
+
+	BST t,tm;
+	node *loc,*par;
+	int key,ch;
+
+	do
+	{
+		cout<<"\n\nMenu :\n1. Create\n2. Display tree (inorder)\n3. Insert"<<endl;
+		cout<<"4. Display levelwise \n5. Search\n6. Delete\n7. Mirror image"<<endl;
+		cout<<"\nEnter your choice: ";
+		cin>>ch;
+		switch(ch)
+		{
+		case 1:
+			t.createBST();
+			break;
+
+		case 2:
+			cout<<"\nThe tree with inorder traversal is: "<<endl;
+			t.display();
+			break;
+
+		case 3:
+			t.insert();
+			break;
+
+		case 4:
+			cout<<"\nThe levelwise traversal is: "<<endl;
+			t.displayLevelWise();
+			break;
+
+		case 5:
+			cout<<"\nEnter a key to search: ";
+			cin>>key;
+			t.search(key,&loc,&par);
+			break;
+
+		case 6:
+			cout<<"\nEnter a key to delete: ";
+			cin>>key;
+			t.delet(key);
+			break;
+
+		case 7:
+			tm = t.createMirror();
+			cout<<"The inorder traversal for mirror image is:"<<endl;
+			tm.display();
+			break;
+
+		case 8:
+			break;
+
+		default:
+			cout<<"\nYou have entered a wrong choice !"<<endl;
+			break;
+		}
+		if(ch==8)
+			break;
+
+	}while(1);
+
+	return 0;
+}
+
---
+++ BinaryTree/binaryTree.cpp
@@ -0,0 +1,287 @@
+//============================================================================
+// Name        : Ass3.cpp
+// Author      : Abhishek
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class Tree;
+
+class node
+{
+	int data;
+	node *left,*right;
+public:
+	friend class Tree;
+
+};
+
+class Tree
+{
+	node *root;
+public:
+	Tree();
+	node* createBinTree();
+	node* insertRec(node*);
+	node* createCopy(node*);
+	Tree copyTree();
+	void insert();
+	void displayLeaf(node*);
+	void display();
+	void inorder(node*);
+	int depth(node*);
+	void height();
+};
+
+Tree::Tree()
+{
+	root = NULL;
+}
+
+node* Tree::createBinTree()
+{
+	root = new node;
+	root->left=root->right=NULL;
+
+	cout<<"Enter data for the root: "<<endl;
+	cin>>root->data;
+
+	cout<<"Insert at left of the root :"<<endl;
+	root->left = insertRec(root->left);
+	cout<<"Insert at right of the root :"<<endl;
+	root->right = insertRec(root->right);
+
+	return root;
+}
+
+node* Tree::insertRec(node *p)
+{
+	int f;
+
+	cout<<"\nPress 1 to insert (0:exit) :";
+	cin>>f;
+
+	if(!f)
+		return p;
+
+	p = new node;
+	p->left=p->right=NULL;
+	cout<<"Enter a data : ";
+	cin>>p->data;
+
+	cout<<"Insert at left of "<<p->data<<endl;
+	p->left = insertRec(p->left);
+	cout<<"Insert at right of "<<p->data<<endl;
+	p->right = insertRec(p->right);
+
+	return p;
+}
+
+void Tree::insert()
+{
+	node *p=root;
+	int ch;
+
+
+	if(root==NULL)
+	{
+		p = new node;
+		p->left=p->right=NULL;
+		cout<<"Enter data for the root: ";
+		cin>>p->data;
+		root = p;
+		return;
+	}
+
+	do
+	{
+		cout<<"Where do you want to inert?\n1.Left of "<<p->data<<"\n2.Right of "<<p->data<<endl;
+		cin>>ch;
+		if(ch==1)
+		{
+			if(p->left==NULL)
+			{
+				p->left=new node;
+				(p->left)->left=(p->left)->right=NULL;
+				cout<<"Enter a data: ";
+				cin>>(p->left)->data;
+				return;
+			}
+			else
+				p = p->left;
+		}
+		else if(ch==2)
+		{
+
+			if(p->right==NULL)
+			{
+				p->right=new node;
+				(p->right)->left=(p->right)->right=NULL;
+				cout<<"Enter a data: ";
+				cin>>(p->right)->data;
+				return;
+			}
+			else
+				p = p->right;
+		}
+
+	}while(1);
+}
+
+Tree Tree::copyTree()
+{
+	Tree t;
+	t.root = createCopy(root);
+	return t;
+}
+
+node* Tree::createCopy(node *p)
+{
+	node *cpy;
+	cpy = new node;
+	cpy=p;
+
+	if(p!=NULL)
+	{
+		cpy->left = createCopy(p->left);
+		cpy->right = createCopy(p->right); 
+	}
+
+	return cpy;
+}
+void Tree::display()
+{
+	int ch;
+
+	do
+	{
+		cout<<"\n\nMenu :\n1. Display root\n2. Display tree (inorder)\n3. Display Leaf Nodes\n4. Exit\n"<<endl;
+		cout<<"Choose display option: ";
+		cin>>ch;
+		switch(ch)
+		{
+		case 1:
+			cout<<"The data of the root is: "<<endl;
+			cout<<root->data;
+			break;
+
+		case 2:
+			cout<<"\nThe in order traversal for tree : "<<endl;
+			inorder(root);
+			break;
+
+		case 3:
+			cout<<"The leaf nodes are: "<<endl;
+			displayLeaf(root);
+			break;
+
+		case 4:
+			break;
+		}
+		if(ch==4)
+			break;
+
+	}while(1);
+}
+
+void Tree::displayLeaf(node *p)
+{
+	int flag=0,flag2=0;
+	if(p->left!=NULL)
+	{
+		displayLeaf(p->left);
+		flag2=1;
+	}
+	else
+	{
+		cout<<p->data<<" ";
+		flag=1;
+	}
+	if(p->right!=NULL)
+	{
+		displayLeaf(p->right);
+	}
+	else
+	{
+		if(!flag && !flag2)
+			cout<<p->data<<" ";
+	}
+}
+
+void Tree::inorder(node *p)
+{
+	if(p!=NULL)
+	{
+		inorder(p->left);
+		cout<<p->data<<" ";
+		inorder(p->right);
+	}	
+}
+
+int Tree::depth(node* t)
+{
+    if (t == NULL)
+        return 0;
+    else
+    {
+        int lDepth = depth(t->left);
+        int rDepth = depth(t->right);
+        return 1+max(lDepth,rDepth);
+    }
+}
+
+void Tree::height()
+{
+	cout<<"The depth of the tree is : "<<depth(root)<<endl;
+}
+
+int main()
+{
+	Tree t,t1;
+	int n,ch;
+
+	do
+	{
+		cout<<"\n\nMain Menu:\n1. Create tree\n2. Insert node\n3. Display\n4. Depth of a tree\n5. Copy tree\n6. Exit"<<endl;
+		cout<<"Enter your choice: "<<endl;
+		cin>>ch;
+		switch(ch)
+		{
+		case 1:
+			t.createBinTree();
+			break;
+
+		case 2:
+			t.insert();
+			break;
+
+		case 3:
+			t.display();
+			break;
+
+		case 4:
+			t.height();
+			break;
+
+		case 5:
+			t1 = t.copyTree();
+			t1.display();
+			break;
+		case 6:
+			break;
+			
+		default:
+			cout<<"You have entered a wrong choice !!"<<endl;
+			break;
+		}
+
+		if(ch==6)
+			break;
+	}while(1);
+
+	return 0;
+}
