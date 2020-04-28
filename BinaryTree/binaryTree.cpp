8d2b25b6bf7630041db0d2b724e5adc455d9222b
abhishek1704 <velankarabhishek@gmail.com>
2020-04-28 10:16:17 +0530

    Update binaryTree.cpp

--- BinaryTree/binaryTree.cpp
+++ BinaryTree/binaryTree.cpp
@@ -1,10 +1,3 @@
-//============================================================================
-// Name        : Ass3.cpp
-// Author      : Abhishek
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
 #include <iostream>
 using namespace std;
